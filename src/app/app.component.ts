440395e8abb4b48d75b59ca0878bc37637c3cb75
kencarl <63323859+kencarl@users.noreply.github.com>
2020-06-04 22:18:39 -0400

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,15 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
- 
           let fetchedSatellites = data.satellites;
-          //let satellite: object;
           for (let i = 0; i < fetchedSatellites.length; i++) {
             let satellite = new Satellite (
               fetchedSatellites[i]["name"],
@@ -26,22 +26,23 @@ export class AppComponent {
               fetchedSatellites[i]["operational"],
             );
              this.sourceList.push(satellite);
-          }
-          
- 
-       }.bind(this));
-    }.bind(this));
+          } this.displayList = this.sourceList.slice(0);
+        }.bind(this));
+     }.bind(this));
 
   }
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+ }
 
 }
-
-// constructor() {
-//   this.sourceList = [
-//     new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-//     new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-//     new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-//     new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-//     new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-//   ];
-// }
